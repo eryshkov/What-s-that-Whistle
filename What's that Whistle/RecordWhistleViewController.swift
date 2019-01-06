023061da963eb20eb4d695cc16aacc8cd44d6cae
Evgeniy Ryshkov <eryshkov@gmail.com>
2019-01-06 11:16:15 +0300

    RecordWhistleViewController is just added

---
+++ What's that Whistle/RecordWhistleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecordWhistleViewController.swift
+//  What's that Whistle
+//
+//  Created by Evgeniy Ryshkov on 06/01/2019.
+//  Copyright © 2019 Evgeniy Ryshkov. All rights reserved.
+//
+
+import UIKit
+
+class RecordWhistleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
