5e9b5f27489959e8fdee1010599af1e018a9e771
Evgeniy Ryshkov <eryshkov@gmail.com>
2019-01-07 20:44:14 +0300

    SubmitViewController is added

--- What's that Whistle/SubmitViewController.swift
+++ What's that Whistle/SubmitViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CloudKit
 
 class SubmitViewController: UIViewController {
 
@@ -17,10 +18,58 @@ class SubmitViewController: UIViewController {
     var status: UILabel!
     var spinner: UIActivityIndicatorView!
     
+    func doSubmission() {
+        
+    }
+    
+    @objc func doneTapped() {
+        _ = navigationController?.popToRootViewController(animated: true)
+    }
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = UIColor.gray
+        
+        stackView = UIStackView()
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = UIStackView.Distribution.fillEqually
+        stackView.alignment = .center
+        stackView.axis = .vertical
+        view.addSubview(stackView)
+        
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        status = UILabel()
+        status.translatesAutoresizingMaskIntoConstraints = false
+        status.text = "Submitting..."
+        status.textColor = UIColor.white
+        status.font = UIFont.preferredFont(forTextStyle: .title1)
+        status.numberOfLines = 0
+        status.textAlignment = .center
+        
+        spinner = UIActivityIndicatorView(style: .whiteLarge)
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.hidesWhenStopped = true
+        spinner.startAnimating()
+        
+        stackView.addArrangedSubview(status)
+        stackView.addArrangedSubview(spinner)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        title = "You're all set!"
+        navigationItem.hidesBackButton = true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        doSubmission()
     }
     
 
